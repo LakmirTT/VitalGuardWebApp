7625ef76676586d31ae8654654bcf907f446a542
LakmirTT <cultistoftirech@protonmail.com>
2024-05-26 16:14:14 +0200

    serializers implemented

--- VitalGuard/serializers.py
+++ VitalGuard/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from VitalGuard.models import Patient, Measurement
+from VitalGuard.models import Patient, Measurement, User, Threshold, Feedback
 
 """
 Form encoded input/output for API with respect to each model
@@ -14,3 +14,21 @@ class MeasurementSerializer(serializers.ModelSerializer):
     class Meta():
         model = Measurement
         fields = ['patient', 'time_taken', 'heart_rate', 'body_temp', 'ox_saturation', 'blood_pressure']
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    will be used for adding users, type and patient are to be assigned by admin
+    """
+    class Meta():
+        model = User
+        fields = ['name', 'username', 'password']
+
+class ThresholdSerializer(serializers.Serializer):
+    class Meta():
+        model = Threshold
+        fields = ['patient', 'time_changed', 'heart_rate', 'body_temp', 'ox_saturation', 'blood_pressure']
+
+class FeedbackSerializer(serializers.Serializer):
+    class Meta():
+        model = Feedback
+        fields = ['patient', 'doctor', 'header', 'text', 'time']
